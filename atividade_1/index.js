e28658616c43a04b1afbe41da523bc6ce934ddfe
joaopedrobarros777 <66282957+joaopedrobarros777@users.noreply.github.com>
2020-06-19 20:08:10 -0300

    atividade 1 feita

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -1,5 +1,16 @@
 // ## Importe a biblioteca ##
+let rs = require("readline-sync")
 
+let voo = rs.questionFloat("Digite o numero do seu voo.")
+
+let fileira = rs.question("Em qual fileira deseja se sentar?")
+
+console.log ("========================")
+
+console.log ("Check-in para o "+voo+" realizado com sucesso.")
+console.log ("Sua fileira é a "+fileira)
+
+console.log ("========================")
 
 
 // ## Faça o código ##
