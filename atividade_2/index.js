f1edf6bf6276b98c0ccb6226b7edafe770738ec6
joaopedrobarros777 <66282957+joaopedrobarros777@users.noreply.github.com>
2020-06-19 20:31:42 -0300

    atividade 2 feita

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -1,5 +1,15 @@
 // ## Importe a biblioteca ##
 
+let rs = require("readline-sync")
 
+let respostaUsuario = rs.question("Voce esta usando mascara?")
+
+let resposta2Usuario = rs.question("Voce esta lavando as maos?")
+
+if (respostaUsuario === "sim") {
+    console.log("voce esta fazendo a coisa certa!")
+} else {
+    console.log("voce esta correndo riscos")
+}
 
 // ## Faça o código ##
