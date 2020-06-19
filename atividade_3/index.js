481c75f13768e19fa19308c648b923d24d965e74
joaopedrobarros777 <66282957+joaopedrobarros777@users.noreply.github.com>
2020-06-19 20:37:18 -0300

    atividade 3 feita

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,12 @@
 // ## Importe a biblioteca ##
 
+let rs = require("readline-sync")
+
+let altura = rs.question("quanto mede a altura do quadrado?")
+
+let largura = rs.question("quanto mede a largura do quadrado?")
+
+console.log("a area do quadrado eh "+altura*largura)
 
 
 // ## Faça o código ##
